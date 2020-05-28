49ad85caa063ec77aa0317c8bdac515e22efdb19
= <al.buhler@gmail.com>
2020-05-28 12:11:03 -0300

    Novos Schemas

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -12,5 +12,7 @@ using SalesWebMvc.Models;
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
-    }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+}
