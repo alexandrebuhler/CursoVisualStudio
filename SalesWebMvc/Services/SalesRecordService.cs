1818bca5ed9dbf65f7f5db1cec6ef0e3a8c05a23
= <al.buhler@gmail.com>
2020-06-05 16:17:13 -0300

    Projeto Concluído

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using SalesWebMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Services
 {
@@ -27,13 +27,32 @@ namespace SalesWebMvc.Services
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
-
             return await result
-                .Include (x => x.Seller)
-                .Include (x => x.Seller.Department)
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
-    }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+
+    }
 }
